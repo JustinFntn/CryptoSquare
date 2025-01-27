f253afd75b19235675f4d1e2da489049ea97cdbd
noupiiii <louisbruche2022@gmail.com>
2025-01-27 11:20:35 +0100

    feat: enhance login page with form validation and styling; update Navbar and default layout

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-[999]">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" class="flex items-center">
           <img src="/img/logo-light-theme.svg" class="h-8 mr-3" alt="Logo">
