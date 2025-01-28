7a8c1d86f08607326516ea5752b27a1a6ca1796d
noupiiii <louisbruche2022@gmail.com>
2025-01-28 15:49:23 +0100

    feat(challenges): enhance challenge display with difficulty filtering and add answer submission area

--- pages/dashboard/challenges/[id].vue
+++ pages/dashboard/challenges/[id].vue
@@ -1,21 +1,35 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { marked } from 'marked'; // Installe marked si ce n'est pas déjà fait : npm install marked
+import { marked } from 'marked';
 
 const markdownContent = ref('');
 
 onMounted(async () => {
-    const rawMd = await $fetch('/challenges/example.md'); // Charger le fichier .md
-    markdownContent.value = marked(rawMd); // Convertir le Markdown en HTML
+    const rawMd = await $fetch('/challenges/example.md');
+    markdownContent.value = marked(rawMd);
 });
 
 definePageMeta({
     layout: 'user'
-})
+});
+
+const value = ref('')
 </script>
 
 <template>
-    <UContainer>
-        <div class="prose max-w-none" v-html="markdownContent"></div>
-    </UContainer>
+    <div class="h-full w-full">
+        <!-- Contenu Markdown avec un padding inférieur pour éviter le chevauchement -->
+        <div class="prose max-w-none p-4 pb-36 md:pb-40" v-html="markdownContent"></div>
+
+        <!-- Élément fixe en bas de la page -->
+        <div
+            class="fixed flex bottom-0 w-full h-28 border-t dark:bg-neutral-900 bg-neutral-100 px-4 py-4 dark:border-neutral-800">
+            <div>
+                <h1 class="font-semibold text-lg">Answers area</h1>
+                <div class="w-96 text-neutral-50 flex gap-2">
+                    <UInput v-model="value" class="text-white-50" />
+                    <UButton label="Submit" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
