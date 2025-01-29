79d2865b95bcc81e136149b36582f0297ceb15d2
noupiiii <louisbruche2022@gmail.com>
2025-01-29 14:59:57 +0100

    feat: enhance dashboard layout and functionality with new components and styles

--- pages/dashboard/groups/new.vue
+++ pages/dashboard/groups/new.vue
@@ -8,14 +8,41 @@
             <UFormGroup label="Squad description">
                 <UTextarea />
             </UFormGroup>
-            <DashboardChallengesGroupsPicturePicker />
-            <UButton label="Create" variant="outline" icon="i-lucide-plus" />
+            <div class="my-4">
+                <h2 class="font-semibold">Squad color</h2>
+                <div class="flex flex-wrap gap-2">
+                    <div v-for="color in colors" :key="color" :style="{ backgroundColor: color }"
+                        class="w-10 h-10 rounded-full cursor-pointer border-2 border-transparent hover:border-gray-500 flex items-center justify-center"
+                        :class="{ 'border-gray-800 dark:border-white border-4': selectedColor === color }"
+                        @click="selectedColor = color">
+                        <span v-if="selectedColor === color" class="text-white text-lg font-bold">✓</span>
+                    </div>
+                </div>
+            </div>
+            <UButton label="Create" block variant="outline" icon="i-lucide-plus" />
         </UContainer>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const colors = [
+    "#FF5733",
+    "#33FF57",
+    "#3357FF",
+    "#F1C40F",
+    "#8E44AD",
+    "#E74C3C",
+    "#2ECC71",
+    "#3498DB",
+    "#F39C12",
+    "#1ABC9C"
+];
+
+const selectedColor = ref(null);
+
 definePageMeta({
     layout: 'user'
-})
+});
 </script>
