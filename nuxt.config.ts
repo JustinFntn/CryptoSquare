25bcc82c012cd2816363415fd14c784040e5933a
Colin Prokopowicz <colin.prokopowicz@c1cloud.ovh>
2025-01-29 09:16:10 +0100

    feat(auth): ajout du middleware clerk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 export default defineNuxtConfig({
-  modules: [
-    "@nuxt/ui",
-    "@clerk/nuxt",
-    "@nuxtjs/tailwindcss",
-    "@pinia/nuxt",
-  ],
+  modules: ["@nuxt/ui", "@clerk/nuxt", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-01-27",
   telemetry: false,
+  clerk: {
+    skipServerMiddleware: true,
+  },
 })
