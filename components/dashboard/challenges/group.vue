056c95bd3a5a95055532a9527a0c8b51749b77a0
n0upi <107274112+noupiiii@users.noreply.github.com>
2025-01-30 22:25:40 +0100

    Revert "feat: enhance dashboard layout and improve sidebar links"

--- components/dashboard/challenges/group.vue
+++ components/dashboard/challenges/group.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="w-full">
-    <div class="my-2">
-      <h1 class="text-xl font-semibold">{{ title }}</h1>
-      <p class="text-neutral-500">{{ subtitle }}</p>
+    <div class="w-full">
+        <div class="my-2">
+            <h1 class="text-xl font-semibold">{{ title }}</h1>
+            <p class="text-neutral-500">{{ subtitle }}</p>
+        </div>
+        <!-- Conteneur avec un défilement horizontal -->
+        <div class="overflow-x-auto">
+            <!-- Flexbox pour les cartes -->
+            <div class="flex gap-3 w-fit py-2">
+                <!-- Boucle sur la liste des défis -->
+                <DashboardChallengesCard v-for="challenge in challengesList" :key="challenge.id"
+                    :title="challenge.title" :difficulty="challenge.difficulty" :subtitle="challenge.subtitle"
+                    :_id="challenge._id" />
+            </div>
+        </div>
     </div>
-    <!-- Conteneur avec un défilement horizontal -->
-    <div class="overflow-x-auto">
-      <!-- Flexbox pour les cartes -->
-      <div class="flex gap-3 w-fit py-2">
-        <!-- Boucle sur la liste des défis -->
-        <DashboardChallengesCard v-for="challenge in challengesList" :key="challenge.id" :title="challenge.title"
-          :difficulty="challenge.difficulty" :subtitle="challenge.subtitle" :_id="challenge._id" />
-      </div>
-    </div>
-  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-  title: {
-    type: String,
-    required: true
-  },
-  subtitle: {
-    type: String,
-  },
-  challengesList: {
-    type: Array,
-    required: true
-  }
+    title: {
+        type: String,
+        required: true
+    },
+    subtitle: {
+        type: String,
+        required: true
+    },
+    challengesList: {
+        type: Array,
+        required: true
+    }
 });
 </script>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,44 +1,8 @@
 <template>
-  <!-- <div class="bg-primary-500 text-neutral-50 p-1">
-    <p>This application is under construction. It may contain some bugs.</p>
-  </div> -->
-  <div class="my-8">
-    <h1 class="text-xl font-semibold text-primary-500">Dashboard</h1>
-    <UContainer>
-      <div>
-        <h2 class="text-lg text-neutral-200"></h2>
-        <DashboardChallengesGroup :title="'Pick up where you left off...'" :challengesList="easyChallenges" />
-      </div>
-    </UContainer>
-    <UContainer>
-      <div class="my-6">
-        <h2 class="text-lg text-neutral-200">News</h2>
-        <ul class="flex gap-4 overflow-x-auto p-1 pb-4 scrollbar-hidden">
-          <li class="snap-start shrink-0 w-64">
-            <UCard>
-              <div class="flex flex-col gap-2">
-                <h1 class="text-lg">Launching the application 🎉</h1>
-                <p class="text-neutral-500">Application launch scheduled for January 30, 2025</p>
-                <UButton label="Read more" class="w-fit" />
-              </div>
-            </UCard>
-          </li>
-        </ul>
-      </div>
-    </UContainer>
-  </div>
+  <div>dashboard.vue</div>
 </template>
-
 <script setup>
-import { useChallengesStore } from '@/stores/challengesStore';
-
 definePageMeta({
   layout: "user",
 })
-const challengesStore = useChallengesStore();
-
-const easyChallenges = computed(() => challengesStore.getChallengesByDifficulty('medium'));
-onMounted(() => {
-  challengesStore.fetchChallenges();
-});
 </script>
