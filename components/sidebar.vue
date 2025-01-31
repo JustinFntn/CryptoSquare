c07b5abf1cfdf00449cd0e349eb51ccfb6c0ff28
BOIS Thimote <thimote.bois@etu.univ-littoral.fr>
2025-01-31 13:51:03 +0100

    fix: typo squad

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -43,7 +43,7 @@ const { user } = useUser()
 const linksTop = ref([
   { label: "Dashboard", icon: "i-lucide-blocks", link: "/dashboard/" },
   { label: "Profile", icon: "i-lucide-user", link: "/dashboard/profile" },
-  { label: "Groups", icon: "i-lucide-waypoints", link: "/dashboard/groups" },
+  { label: "Squads", icon: "i-lucide-waypoints", link: "/dashboard/groups" },
   { label: "Challenges", icon: "i-lucide-brain-circuit", link: "/dashboard/challenges" },
   { label: "Create a challenge", icon: "i-lucide-pencil-line", link: "/dashboard/challenges/challenge-creator" },
 ])
